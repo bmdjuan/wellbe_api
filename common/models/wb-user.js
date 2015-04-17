b3239862bf49f60d867772cb1bfe8b7b9b2798ef
bmdjuan <beatriz.martin@axa-groupsolutions.com>
2015-04-17 12:54:34 +0200

    [UPDATE] Integration loopback-faq-user-management in the project

--- common/models/wb-user.js
+++ common/models/wb-user.js
@@ -1,23 +1,76 @@
-module.exports = function(WbUser) {
+var config = require('../../server/config.json');
+var path = require('path');
+
+module.exports = function(WBUser) {
 //Relations : http://docs.strongloop.com/display/public/LB/Tutorial%3A+model+relations
 
   // Setup validations
-  WbUser.validatesPresenceOf('guid','name', 'email','language','password');
-  WbUser.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
-  WbUser.validatesUniquenessOf('email', {message: 'email is not unique'});
+  WBUser.validatesPresenceOf('guid','name', 'email','language','password');
+  WBUser.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
+  WBUser.validatesUniquenessOf('email', {message: 'email is not unique'});
 
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  WbUser.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
-  if (!(WbUser.settings.realmRequired || UserModel.settings.realmDelimiter)) {
-    WbUser.validatesUniquenessOf('email', {message: 'Email already exists'});
-    WbUser.validatesUniquenessOf('guid', {message: 'User already exists'});
+  WBUser.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
+  if (!(WBUser.settings.realmRequired || WBUser.settings.realmDelimiter)) {
+    WBUser.validatesUniquenessOf('email', {message: 'Email already exists'});
+    WBUser.validatesUniquenessOf('guid', {message: 'User already exists'});
   }
 
-  user.isValid(function (valid) {
+  WBUser.isValid(function (valid) {
     if (!valid) {
-      user.errors // hash of errors {attr: [errmessage, errmessage, ...], attr: ...}
+      WBUser.errors // hash of errors {attr: [errmessage, errmessage, ...], attr: ...}
     }
   })
+
+
+  //send verification email after registration
+  WBUser.afterRemote('create', function(context, user) {
+    console.log('> user.afterRemote triggered');
+
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: 'noreply@loopback.com',
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
+
+    WBUser.verify(options, function(err, response) {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      console.log('> verification email sent:', response);
+
+      context.res.render('response', {
+        title: 'Signed up successfully',
+        content: 'Please check your email and click on the verification link '
+        + 'before logging in.',
+        redirectTo: '/',
+        redirectToLinkText: 'Log in'
+      });
+    });
+  });
+
+  //send password reset link when requested
+  WBUser.on('resetPasswordRequest', function(info) {
+    var url = 'http://' + config.host + ':' + config.port + '/reset-password';
+    var html = 'Click <a href="' + url + '?access_token=' + info.accessToken.id
+      + '">here</a> to reset your password';
+
+    WBUser.app.models.Email.send({
+      to: info.email,
+      from: info.email,
+      subject: 'Password reset',
+      html: html
+    }, function(err) {
+      if (err) return console.log('> error sending password reset email');
+      console.log('> sending password reset email to:', info.email);
+    });
+  });
 
 };
